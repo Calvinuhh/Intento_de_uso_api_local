e46248fc37c37cdbcb4cf72f3a022d46d2e699fc
Calvin Ulis Howard Howard <Calvin.uhh@gmail.com>
2023-12-21 17:07:48 -0500

    Pequeños cambios

--- agregar.js
+++ agregar.js
@@ -23,3 +23,28 @@ formulario.addEventListener("submit", (event) => {
   });
   alert(`Has agregado "${nombrePostre}" exitosamente`);
 });
+
+
+
+//  Para agregar postres escribir unicamente el nombre del postre y 
+//  el numero de la imagen, ejemplo: 3312, se esta concatenado 
+//  el nombre del archivo de la imagen, solo hay que agregar el numero
+
+
+// {
+//   "name": "Postre de vainilla",
+//   "id": 4,
+//   "img": "./pics/IMG_3312.PNG"
+// }
+
+// {
+//   "name": "Postre de chocolate",
+//   "id": 5,
+//   "img": "./pics/IMG_3322.PNG"
+// }
+
+// {
+//   "name": "Mufins navideños",
+//   "id": 6,
+//   "img": "./pics/IMG_3323.PNG"
+// }
